c08b4cf84a7228204ff778ece054db1dbf73c409
Lucas Figueroa <lucas.200061@gmail.com>
2022-12-23 12:17:58 -0300

    workbook-1

--- src/components/MiNavBar/NavBar.jsx
+++ src/components/MiNavBar/NavBar.jsx
@@ -3,26 +3,26 @@ import './styles.css'
 import NavWidget from '../NavWidget/NavWidget';
 function NavBar() {
   return (
-    <nav class="navbar navbar-dark navbar-expand-lg bg-dark">
-    <div class="container-fluid">
-      <a class="logo" href="/#">Ozeer</a>
+    <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
+    <div className="container-fluid">
+      <a className="logo" href="/#">Ozeer</a>
 
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navSupp"
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navSupp"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+        <span className="navbar-toggler-icon"></span>
       </button>
 
-      <div class="collapse navbar-collapse" id="navSupp">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="item">
-            <a class="item" href="/#">Inicio</a>
+      <div className="collapse navbar-collapse" id="navSupp">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="item">
+            <a className="item" href="/#">Inicio</a>
           </li>
-          <li class="item" >
-            <a class="item" href="/#">Productos</a>
+          <li className="item" >
+            <a className="item" href="/#">Productos</a>
           </li>
 
-          <li class="item">
-            <a class="item" href="/#">Galeria</a>
+          <li className="item">
+            <a className="item" href="/#">Galeria</a>
           </li>
         </ul>
 
--- src/containers/ItemListContainer/ItemListContainer.jsx
+++ src/containers/ItemListContainer/ItemListContainer.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import ItemCount from '../../components/itemCount/ItemCount'
 
 function ItemListContainer({greeting}) {
   return (
-    <h1 className='container-fluid text-center mt-3 p-3'>{greeting}</h1>
+    <>
+      <h1 className='container-fluid text-center mt-3 p-3'>{greeting}</h1>
+      <ItemCount stock={4} initial={1}></ItemCount>
+      
+    </>
   )
 }
 
